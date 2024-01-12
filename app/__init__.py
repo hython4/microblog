84bed3b0a3b9288f1ef81435bcf0f18efa6723fa
hython4 <hython4@gmail.com>
2024-01-12 09:36:15 +0000

    create init db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from config import Config
 from flask import Flask
+from flask_migrate import Migrate  # noqa: F401
+from flask_sqlalchemy import SQLAlchemy  # noqa: F401
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # noqa: F401, E402
+from app import models, routes  # noqa: F401, E402
